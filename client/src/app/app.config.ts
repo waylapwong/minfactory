051e11ee5bbbb705660c93b4ebcc7228a25b09a1
Way-Lap Wong <waylapwong@gmail.com>
2025-09-23 08:59:34 +0200

    deps: install some deps

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
