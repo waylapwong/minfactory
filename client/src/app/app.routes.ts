bacba47a296ad3544238770a360f0c1b10c99a3b
Way-Lap Wong <waylapwong@gmail.com>
2025-09-23 16:40:11 +0200

    feat: add home component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const ROUTES: Routes = [];
+export enum Paths {
+  Root = '',
+  Home = 'home',
+  Any = '**'
+}
+
+enum Titles {
+  MinFactory = 'minFactory'
+}
+
+export const ROUTES: Routes = [
+  {
+    path: Paths.Root,
+    redirectTo: Paths.Home,
+    pathMatch: 'full'
+  },
+  {
+    path: Paths.Home,
+    title: Titles.MinFactory,
+    loadComponent: () =>
+      import('./features/minfactory/pages/home/home.component').then((m) => m.HomeComponent)
+  },
+  {
+    path: Paths.Any,
+    redirectTo: Paths.Root,
+    pathMatch: 'full'
+  }
+];
