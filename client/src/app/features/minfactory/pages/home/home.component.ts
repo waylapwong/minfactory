a1f68373ae4bfdf3676aef61b79de10f50b8e5c8
Way-Lap Wong <waylapwong@gmail.com>
2025-10-04 13:22:41 +0200

    feat: add text for home page

--- client/src/app/features/minfactory/pages/home/home.component.ts
+++ client/src/app/features/minfactory/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { H1Component } from '../../../../shared/components/h1/h1.component';
+import { H2Component } from '../../../../shared/components/h2/h2.component';
 import { LogoComponent } from '../../../../shared/components/logo/logo.component';
 
 @Component({
   selector: 'min-home',
-  imports: [LogoComponent],
+  imports: [LogoComponent, H1Component, H2Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
