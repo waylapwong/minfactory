e02409ed91d70c55585eb15c17acf6d18f69d171
Way-Lap Wong <waylapwong@gmail.com>
2025-10-06 01:07:21 +0200

    chore: fix lint findings

--- client/src/app/features/minrps/minrps.component.ts
+++ client/src/app/features/minrps/minrps.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-minrps',
+  selector: 'min-minrps',
   imports: [RouterOutlet],
   templateUrl: './minrps.component.html',
   styleUrls: ['./minrps.component.scss']
--- client/src/app/features/minrps/pages/minrps-menu/minrps-menu.component.ts
+++ client/src/app/features/minrps/pages/minrps-menu/minrps-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-minrps-menu',
+  selector: 'min-minrps-menu',
   templateUrl: './minrps-menu.component.html',
   styleUrls: ['./minrps-menu.component.scss']
 })
