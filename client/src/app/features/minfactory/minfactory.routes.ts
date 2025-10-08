fce2a067bdec4ead067d673107d6397c0b3b615d
Way-Lap Wong <waylapwong@gmail.com>
2025-10-08 20:17:45 +0200

    feat: remove minplay page

--- client/src/app/features/minfactory/minfactory.routes.ts
+++ client/src/app/features/minfactory/minfactory.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 export enum MinFactoryPath {
   MinApps = 'apps',
-  MinPlay = 'play',
   Root = '',
 }
 
@@ -14,9 +13,5 @@ export const MINFACTORY_ROUTES: Routes = [
   {
     path: MinFactoryPath.MinApps,
     loadComponent: () => import('./pages/apps/apps.component').then((m) => m.AppsComponent),
-  },
-  {
-    path: MinFactoryPath.MinPlay,
-    loadComponent: () => import('./pages/play/play.component').then((m) => m.PlayComponent),
   },
 ];
