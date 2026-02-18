ac82b93a81d3b2cd55e529fd377592e9699d3fb2
Way-Lap Wong <waylapwong@gmail.com>
2026-02-18 20:47:08 +0100

    test: remove unnecessary tests

--- server/src/shared/enums/namespace.enum.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Namespace } from './namespace.enum';
-
-describe('Namespace', () => {
-  it('should have MinRps namespace defined', () => {
-    expect(Namespace.MinRps).toBe('minrps');
-  });
-});
