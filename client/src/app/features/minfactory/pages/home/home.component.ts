2a23e090ffa5e61727a51cf757aa906976e2a4f9
Way-Lap Wong <waylapwong@gmail.com>
2026-03-08 10:41:59 +0100

    feat: update home component layout and add button functionality

--- client/src/app/features/minfactory/pages/home/home.component.ts
+++ client/src/app/features/minfactory/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { H1Component } from '../../../../shared/components/h1/h1.component';
 import { LogoComponent } from '../../../../shared/components/logo/logo.component';
+import { Color } from '../../../../shared/enums/color.enum';
 
 @Component({
   selector: 'minfactory-home',
-  imports: [LogoComponent, H1Component],
+  imports: [LogoComponent, H1Component, ButtonComponent, LogoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public readonly Color = Color;
+}
