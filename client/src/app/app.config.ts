90721477be6573b7a4f67471db1c63b21dcd3dad
Way-Lap Wong <waylapwong@gmail.com>
2026-03-13 13:36:20 +0100

    chore: change lin ending to lf

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { registerLocaleData } from '@angular/common';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import localeDe from '@angular/common/locales/de';
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { ENVIRONMENT } from '../environments/environment';
 import { APP_ROUTES } from './app.routes';
