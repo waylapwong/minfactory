47d3cc8c3b8e66e4950f29bdd87a191980bc322e
Way-Lap Wong <waylapwong@gmail.com>
2026-03-20 18:33:27 +0100

    Feature: Refactor layout by adding dividers and improving component structure

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { FooterComponent } from './core/components/footer/footer.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { ContextService } from './core/services/context.service';
 import { AppName } from './shared/enums/app-name.enum';
+import { DividerComponent } from './shared/components/divider/divider.component';
 
 @Component({
   selector: 'min-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DividerComponent],
 })
 export class AppComponent {
   public readonly isInMinFactory: Signal<boolean> = computed(() => this.contextService.app() === AppName.MinFactory);
