c4828bcb6287954d247214d7dcbde36878642771
Way-Lap Wong <waylapwong@gmail.com>
2026-03-27 23:12:16 +0100

    feat: remove unused play button and related import from MinPokerHome component

--- client/src/app/features/minpoker/pages/minpoker-home/minpoker-home.component.ts
+++ client/src/app/features/minpoker/pages/minpoker-home/minpoker-home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RoutingService } from '../../../../core/routing/routing.service';
-import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { CardButtonComponent } from '../../../../shared/components/card-button/card-button.component';
 import { H1Component } from '../../../../shared/components/h1/h1.component';
 import { H2Component } from '../../../../shared/components/h2/h2.component';
@@ -12,7 +11,7 @@ import { Color } from '../../../../shared/enums/color.enum';
   templateUrl: './minpoker-home.component.html',
   styleUrls: ['./minpoker-home.component.scss'],
   host: { class: 'block h-full w-full' },
-  imports: [ButtonComponent, CardButtonComponent, H2Component, LogoComponent, H1Component],
+  imports: [ CardButtonComponent, H2Component, LogoComponent, H1Component],
 })
 export class MinPokerHomeComponent {
   public readonly Color: typeof Color = Color;
